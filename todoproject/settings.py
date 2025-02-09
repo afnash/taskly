9780c9d4bc874d6e5242616cbdb96b57d873fcb3
afnash <iamafnash@gmail.com>
2025-02-09 10:58:12 +0530

    for vercel-5

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -80,9 +80,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER' : config('DB_USER'),
-        'PASSWORD':config('DB_PASSWORD'),
-        'HOST':config('DB_HOST'),
-        'PORT':config('DB_PORT'),  
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),  
     }
 }
 
